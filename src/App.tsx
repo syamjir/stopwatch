40a00ea8ffa304f7b05fb86985737303ef999ba7
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-04 06:28:50 +0530

    add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import "./App.css";
 
-
 function App() {
-  const startHandle = () => {};
-  const stopHandle = () => {};
+  const startHandle = () => {
+
+  };
+  const stopHandle = () => {
+
+  };
+  const resetHandle = () => {
+    
+  };
+
+  const formatTime=()=>{
+      return "00:00:00"
+  };
+
   return (
-    <div className="container">
+    <div className="stopwatch">
       <h1>STOP WATCH</h1>
       <div>
-        <h2>00:00:00</h2>
+        <div className="display">
+          <h2>{formatTime()}</h2>
+        </div>
 
         <div>
-          <button onClick={startHandle}>Start</button>
-          <button onClick={stopHandle}>Stop</button>
+          <button className="btn" onClick={startHandle}>
+            Start
+          </button>
+          <button className="btn" onClick={stopHandle}>
+            Stop
+          </button>
+          <button className="btn" onClick={resetHandle}>
+            Reset
+          </button>
         </div>
       </div>
     </div>
